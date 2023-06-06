4232e3149c4664494ca890345b82a099c1f33b4c
NorielSylvire <noriel.sylvire@gmail.com>
2023-06-06 22:55:38 +0200

    Updated printnum, updated libft submodule

--- src/conversions/printnum.c
+++ src/conversions/printnum.c
@@ -6,7 +6,7 @@
 /*   By: fhongu <fhongu@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 20:53:49 by fhongu            #+#    #+#             */
-/*   Updated: 2023/06/05 22:19:23 by fhongu           ###   ########.fr       */
+/*   Updated: 2023/06/06 20:55:46 by fhongu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,36 @@
 void	printnum(int *counter, t_bflags bflags, int num)
 {
 	char	*str;
+	long	n;
 	int		bigger;
 	int		len;
 	int		neg;
 
+	n = (long) num;
 	neg = 0;
-	if (num < 0)
+	if (n < 0 || bflags.plus)
 	{
-		neg = 1;
-		num *= -1;
+		if (n < 0)
+		{
+			neg = 1;
+			n *= -1;
+		}
+		bflags.min_width--;
 	}
-	str = ft_itoa(num);
+	else if (bflags.plus)
+	str = ft_itoa(n);
 	len = (int) ft_strlen(str);
 	bigger = len;
 	if (bflags.precision > len)
 		bigger = bflags.precision;
-	bflags.min_width -= neg;
 	if (!bflags.minus)
 	{
 		while (bflags.min_width-- > bigger)
 			*counter += ft_putchar_fd(' ', 1);
 		if (neg)
 			*counter += ft_putchar_ft('-', 1);
+		else if (bflags.plus)
+			*counter += ft_putchar_fd('+', 1);
 		while (bflags.precision-- > len)
 			*counter += ft_putchar_fd('0', 1);
 		*counter += ft_putstr_fd(str, 1);
